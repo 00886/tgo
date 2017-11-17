86c617edf88a5330eaeaad98b4a01d1a1e35622a
swift <175072975@qq.com>
2017-11-17 20:41:08 +0800

    2017-11-17 20:40

---
+++ src/main/java/cn/tgo/core/bean/ImgUrl.java
@@ -0,0 +1,30 @@
+package cn.tgo.core.bean;
+
+
+public class ImgUrl {
+    private String path;
+    private String url;
+
+    public String getPath() {
+        System.out.println(11111111);
+        System.out.println(11111111);
+        System.out.println(11111111);System.out.println(11111111);System.out.println(11111111);
+        System.out.println(11111111);
+        int i = 1+2;
+        System.out.println(i);
+        System.out.println(i);
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
