53251bbfcde3565851d1103c536c36033e563981
swift <175072975@qq.com>
2017-11-17 20:42:57 +0800

    2017-11-17 20:42

--- src/main/java/cn/tgo/core/bean/ImgUrl.java
+++ src/main/java/cn/tgo/core/bean/ImgUrl.java
@@ -6,13 +6,6 @@ public class ImgUrl {
     private String url;
 
     public String getPath() {
-        System.out.println(11111111);
-        System.out.println(11111111);
-        System.out.println(11111111);System.out.println(11111111);System.out.println(11111111);
-        System.out.println(11111111);
-        int i = 1+2;
-        System.out.println(i);
-        System.out.println(i);
         return path;
     }
 
